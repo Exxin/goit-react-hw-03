1843dfafdca32d24c76df508a274ff3d9b0befa6
Exxin <exxinblood@gmail.com>
2024-04-10 19:21:20 +0300

    d

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css';
-
-const Button = ({ text = "Default text", onUpgrade  }) => {
-    return (
-        <button className={styles.button} onClick={ () => onUpgrade(text) }>{ text }</button>
-    )
-}
-
-export default Button;
